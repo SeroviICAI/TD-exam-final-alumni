4d7354e9baf54f23c22c3570da29a3e57e33fd8f
Sergio Rodríguez Vidal <202113624@alu.comillas.edu>
2025-05-05 08:39:28 +0200

    First Commit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../../icai2025-5f9879654f17.json")
+  credentials = file("../../gcp-sa-key.json")
 
   project = var.gcp-project
   region  = var.gcp-region
